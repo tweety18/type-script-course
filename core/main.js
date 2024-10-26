5c20c9e3f26042e2453b98d95afc4851ba509654
Alona <alona.smetanina@raiffeisen.ua>
2024-10-26 18:30:12 +0200

    add next hw

--- core/main.js
+++ core/main.js
@@ -1,3 +1,42 @@
+// ДЗ 5. TS: Deep Copy
+// Напиши функцію для рекурсивного копіювання об'єкта, враховуючи вкладені об'єкти та масиви.
+function deepClone(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map((item) => deepClone(item));
+    }
+    else if (typeof obj === 'object' && obj !== null) {
+        const newObj = {};
+        for (let key in obj) {
+            const item = obj[key];
+            if (typeof item === 'object') {
+                newObj[key] = deepClone(item);
+            }
+            else {
+                newObj[key] = obj[key];
+            }
+        }
+        return newObj;
+    }
+    else {
+        return obj;
+    }
+}
+const myObj = [1, 2, null];
+//     {
+//     a: 1,
+//     b: [1, null, 3],
+//     c: 3,
+//     d: [
+//         4,
+//         [5,6,7],
+//         {key: '123'},
+//         {key: '456'},
+//     ]
+// };
+const copy = deepClone(myObj);
+console.log(myObj);
+console.log(copy);
+console.log(myObj === copy);
 // ДЗ 8. TS Перевірка рядка на заборонені слова з виділенням
 // Напиши функцію highlightForbiddenWords, яка приймає рядок і масив заборонених слів.
 // Якщо знаходить у рядку заборонене слово, то обгортає його в тег <del>, який буде закреслювати це слово.
@@ -15,11 +54,13 @@ function checkInputs() {
 }
 sentence.addEventListener('input', checkInputs);
 forbiddenWords.addEventListener('input', checkInputs);
+function splitString(text) {
+    return text.toLowerCase().split(/[ ,]+/).filter(Boolean);
+}
 function highlightForbiddenWords(text, words) {
-    const textArr = text.toLowerCase().split(/[ ,]+/).filter(Boolean);
-    const forbArr = words.toLowerCase().split(/[ ,]+/).filter(Boolean);
-    const newArr = textArr.map((item) => forbArr.includes(item) ? `<del>${item}</del>` : item).join(' ');
+    const textArr = splitString(text);
+    const newArr = textArr.map((item) => words.includes(item) ? `<del>${item}</del>` : item).join(' ');
     newElement.innerHTML = newArr;
     container.appendChild(newElement);
 }
-checkBtn.addEventListener('click', () => highlightForbiddenWords(sentence.value, forbiddenWords.value));
+checkBtn.addEventListener('click', () => highlightForbiddenWords(sentence.value, splitString(forbiddenWords.value)));
--- core/main.ts
+++ core/main.ts
@@ -1,3 +1,44 @@
+// ДЗ 5. TS: Deep Copy
+// Напиши функцію для рекурсивного копіювання об'єкта, враховуючи вкладені об'єкти та масиви.
+
+function deepClone(obj: any): any {
+    if (Array.isArray(obj)) {
+        return obj.map((item: any) => deepClone(item));
+    } else if (typeof obj === 'object' && obj !==null) {
+        const newObj: any = {};
+        for (let key in obj) {
+            const item: any = obj[key];
+            if (typeof item === 'object') {
+                newObj[key] = deepClone(item);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+        return newObj;
+    }
+    else {
+        return obj;
+    }
+}
+
+const myObj: any = [1,2, null]
+//     {
+//     a: 1,
+//     b: [1, null, 3],
+//     c: 3,
+//     d: [
+//         4,
+//         [5,6,7],
+//         {key: '123'},
+//         {key: '456'},
+//     ]
+// };
+const copy: any = deepClone(myObj);
+console.log(myObj);
+console.log(copy);
+console.log(myObj === copy);
+
+
 // ДЗ 8. TS Перевірка рядка на заборонені слова з виділенням
 // Напиши функцію highlightForbiddenWords, яка приймає рядок і масив заборонених слів.
 // Якщо знаходить у рядку заборонене слово, то обгортає його в тег <del>, який буде закреслювати це слово.
